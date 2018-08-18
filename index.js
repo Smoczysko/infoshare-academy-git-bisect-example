951d61a15055be13d607b1c972f5c8c5fb79ad9c
Łukasz Rybka <darkstariw@gmail.com>
2018-08-18 21:24:36 +0200

    Error handling.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 #!/usr/bin/env node
 
 const addNumbers = (a, b) => +a + +b;
-const divideNumbers = (a, b) => +a / +b;
+const divideNumbers = (a, b) => {
+    if (+b === 0) {
+        throw "Second number cannot be 0!";
+    }
+
+    return +a / +b;
+};
 
 const arguments = process.argv.slice(2);
 let value;
 
-switch (arguments[0]) {
-    case 'add':
-        value = addNumbers(arguments[1], arguments[2]);
-        break;
-    case 'div':
-        value = divideNumbers(arguments[1], arguments[2]);
-        break;
-}
+try {
+    switch (arguments[0]) {
+        case 'add':
+            value = addNumbers(arguments[1], arguments[2]);
+            break;
+        case 'div':
+            value = divideNumbers(arguments[1], arguments[2]);
+            break;
+    }
 
-console.log('Result: ', value);
+    console.log('Result: ', value);
+} catch (e) {
+    console.error('Error occurred during calculating the result.', e);
+
+    process.exit(1);
+}
